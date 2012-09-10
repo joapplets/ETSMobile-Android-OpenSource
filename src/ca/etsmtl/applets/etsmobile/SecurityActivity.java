ce7a6d09b07d3cf7c5a281ee11099fecede40101
Philippe David <philippe.david0@gmail.com>
2012-09-09 20:40:18 -0400

    secu layout

--- src/ca/etsmtl/applets/etsmobile/SecurityActivity.java
+++ src/ca/etsmtl/applets/etsmobile/SecurityActivity.java
@@ -6,26 +6,56 @@ import java.util.Locale;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.etsmt.applets.etsmobile.views.MyMapMarker;
 import com.etsmt.applets.etsmobile.views.NavBar;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
 public class SecurityActivity extends MapActivity {
 
+	// http://stackoverflow.com/questions/10167819/android-geopoint-from-location-locality
+	public static Address searchLocationByName(final Context context,
+			final String locationName) {
+		final Geocoder geoCoder = new Geocoder(context, Locale.getDefault());
+		Address ad = null;
+		try {
+			final List<Address> addresses = geoCoder.getFromLocationName(
+					locationName, 1);
+			for (final Address address : addresses) {
+				new GeoPoint((int) (address.getLatitude() * 1E6),
+						(int) (address.getLongitude() * 1E6));
+				address.getAddressLine(1);
+				ad = address;
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		return ad;
+	}
+
 	private NavBar navBar;
 	private ListView listView;
+
 	private MapView mapView;
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 
 	@Override
 	protected void onCreate(final android.os.Bundle savedInstanceState) {
@@ -36,59 +66,57 @@ public class SecurityActivity extends MapActivity {
 		navBar = (NavBar) findViewById(R.id.navBar1);
 		navBar.hideRightButton();
 
-		listView = (ListView) findViewById(R.id.listView1);
-		listView.setAdapter(new ArrayAdapter<String>(this,
-				R.layout.simple_list_item_1, getResources().getStringArray(
-						R.array.secu_urgence)));
+		listView = (ListView) findViewById(android.R.id.list);
+
+		final ViewGroup viewGroup = (ViewGroup) getLayoutInflater().inflate(
+				R.layout.secu_list_header,
+				(ViewGroup) findViewById(R.id.secu_list_header_layout));
+		listView.addHeaderView(viewGroup, null, false);
+
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(final AdapterView<?> arg0, final View arg1,
+					final int arg2, final long arg3) {
 
-				Intent intent = new Intent(getApplicationContext(),
-						UrgenceActivity.class);
+				// Intent intent = new Intent(getApplicationContext(),
+				// UrgenceActivity.class);
+				// startActivity(intent);
+
+				final Intent intent = new Intent(Intent.ACTION_VIEW, Uri
+						.parse("android.resource://" + getPackageName() + "/"
+								+ R.raw.odeur_suspecte_et_fuite_gaz_2009_04_01));
+				intent.setType("application/pdf");
 				startActivity(intent);
 			}
 		});
+		listView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, getResources().getStringArray(
+						R.array.secu_urgence)));
 
 		mapView = (MapView) findViewById(R.id.mapview);
-		LayoutParams params = new LayoutParams(100, 300);
-		mapView.setLayoutParams(params);
-		
+
+		final Drawable drawable = getResources().getDrawable(R.drawable.icon);
+		final MyMapMarker markers = new MyMapMarker(drawable, this);
+
 		final MapController controller = mapView.getController();
-		
-		Address adress = searchLocationByName(this,
+
+		final Address adress = SecurityActivity.searchLocationByName(this,
 				"École de Technologie Supérieure");
-		GeoPoint geo = new GeoPoint((int) (adress.getLatitude() * 1E6),
-				(int) (adress.getLongitude() * 1E6));
-		 controller.setCenter(geo);
-		controller.setZoom(12);
-		controller.animateTo(geo);
-	}
+		if (adress != null) {
 
-	// http://stackoverflow.com/questions/10167819/android-geopoint-from-location-locality
-	public static Address searchLocationByName(Context context,
-			String locationName) {
-		Geocoder geoCoder = new Geocoder(context, Locale.getDefault());
-		GeoPoint gp = null;
-		Address ad = null;
-		try {
-			List<Address> addresses = geoCoder.getFromLocationName(
-					locationName, 1);
-			for (Address address : addresses) {
-				gp = new GeoPoint((int) (address.getLatitude() * 1E6),
-						(int) (address.getLongitude() * 1E6));
-				address.getAddressLine(1);
-				ad = address;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			final GeoPoint geo = new GeoPoint(
+					(int) (adress.getLatitude() * 1E6),
+					(int) (adress.getLongitude() * 1E6));
+
+			final OverlayItem items = new OverlayItem(geo,
+					"École de Technologie SUpérieur", "");
+			markers.addOverlay(items);
+
+			mapView.getOverlays().add(markers);
+
+			controller.setCenter(geo);
+			controller.setZoom(18);
+			controller.animateTo(geo);
 		}
-		return ad;
-	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		return false;
 	}
 }
--- src/ca/etsmtl/applets/etsmobile/UrgenceActivity.java
+++ src/ca/etsmtl/applets/etsmobile/UrgenceActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class UrgenceActivity extends Activity {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.urgence);
 	}
