b7837f6b53edaaf944bb87000c43a050bbe42397
Philippe David <philippe.david0@gmail.com>
2013-01-11 14:33:24 -0500

    modif nouvelles

--- src/ca/etsmtl/applets/etsmobile/ETSMobileApp.java
+++ src/ca/etsmtl/applets/etsmobile/ETSMobileApp.java
@@ -1,5 +1,6 @@
 package ca.etsmtl.applets.etsmobile;
 
+import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
@@ -7,9 +8,9 @@ import android.app.Application;
 @ReportsCrashes(formUri = "http://www.bugsense.com/api/acra?api_key=4a893e6b", formKey = "")
 public class ETSMobileApp extends Application {
 
-	@Override
-	public void onCreate() {
-		// ACRA.init(this);
-		super.onCreate();
-	}
+    @Override
+    public void onCreate() {
+	ACRA.init(this);
+	super.onCreate();
+    }
 }
--- src/ca/etsmtl/applets/etsmobile/adapters/NewsCursorAdapter.java
+++ src/ca/etsmtl/applets/etsmobile/adapters/NewsCursorAdapter.java
@@ -2,6 +2,7 @@ package ca.etsmtl.applets.etsmobile.adapters;
 
 import java.text.SimpleDateFormat;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
@@ -17,81 +18,74 @@ import ca.etsmtl.applets.etsmobile.tools.db.NewsTableHelper;
 
 public class NewsCursorAdapter extends CursorAdapter {
 
-	public static class ViewHolder {
-		TextView title, date, description, logo;
+    public static class ViewHolder {
+	TextView title, date, description, logo;
+    }
+
+    private String source, title, description;
+    private final Drawable webLogo, facebookLogo, twitterLogo, interfaceLogo;
+
+    @SuppressLint("SimpleDateFormat")
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMMMMMMMM yyyy");
+
+    public NewsCursorAdapter(final Context context, final Cursor c, final int flags) {
+	super(context, c, flags);
+	webLogo = context.getResources().getDrawable(R.drawable.news_background_ets);
+	facebookLogo = context.getResources().getDrawable(R.drawable.news_background_facebookets);
+	twitterLogo = context.getResources().getDrawable(R.drawable.news_background_twitterets);
+
+	interfaceLogo = context.getResources().getDrawable(R.drawable.news_background_twitterets);
+    }
+
+    @Override
+    public void bindView(final View view, final Context context, final Cursor cursor) {
+
+	ViewHolder holder = (ViewHolder) view.getTag(R.string.viewholdercontenttag);
+	if (holder == null) {
+	    holder = new ViewHolder();
+	    holder.title = (TextView) view.findViewById(R.id.newsListItemTitle);
+	    holder.date = (TextView) view.findViewById(R.id.newsListItemDate);
+	    holder.description = (TextView) view.findViewById(R.id.newsListItemDescription);
+	    holder.logo = (TextView) view.findViewById(R.id.newsListItemLogo);
+	    view.setTag(R.string.viewholdercontenttag, holder);
 	}
 
-	private String source, title, description;
-	private final Drawable webLogo, facebookLogo, twitterLogo;
+	title = cursor.getString(cursor.getColumnIndex(NewsTableHelper.NEWS_TITLE));
+	holder.title.setText(Html.fromHtml(title));
 
-	private final SimpleDateFormat dateFormat = new SimpleDateFormat(
-			"dd MMMMMMMMMM yyyy");
+	holder.date.setText(dateFormat.format(cursor.getLong(cursor
+		.getColumnIndex(NewsTableHelper.NEWS_DATE))));
 
-	public NewsCursorAdapter(final Context context, final Cursor c,
-			final int flags) {
-		super(context, c, flags);
-		webLogo = context.getResources().getDrawable(
-				R.drawable.news_background_ets);
-		facebookLogo = context.getResources().getDrawable(
-				R.drawable.news_background_facebookets);
-		twitterLogo = context.getResources().getDrawable(
-				R.drawable.news_background_twitterets);
+	description = cursor.getString(cursor.getColumnIndex(NewsTableHelper.NEWS_DESCRIPTION));
+	if (description.length() > 200) {
+	    holder.description.setText(Html.fromHtml(description.substring(0, 180)));
+	} else {
+	    holder.description.setText(Html.fromHtml(description));
 	}
 
-	@Override
-	public void bindView(final View view, final Context context,
-			final Cursor cursor) {
-
-		ViewHolder holder = (ViewHolder) view
-				.getTag(R.string.viewholdercontenttag);
-		if (holder == null) {
-			holder = new ViewHolder();
-			holder.title = (TextView) view.findViewById(R.id.newsListItemTitle);
-			holder.date = (TextView) view.findViewById(R.id.newsListItemDate);
-			holder.description = (TextView) view
-					.findViewById(R.id.newsListItemDescription);
-			holder.logo = (TextView) view.findViewById(R.id.newsListItemLogo);
-			view.setTag(R.string.viewholdercontenttag, holder);
-		}
-
-		title = cursor.getString(cursor
-				.getColumnIndex(NewsTableHelper.NEWS_TITLE));
-		holder.title.setText(Html.fromHtml(title));
-
-		holder.date.setText(dateFormat.format(cursor.getLong(cursor
-				.getColumnIndex(NewsTableHelper.NEWS_DATE))));
-
-		description = cursor.getString(cursor
-				.getColumnIndex(NewsTableHelper.NEWS_DESCRIPTION));
-		if (description.length() > 200) {
-			holder.description.setText(Html.fromHtml(description.substring(0,
-					180)));
-		} else {
-			holder.description.setText(Html.fromHtml(description));
-		}
-
-		source = cursor.getString(cursor
-				.getColumnIndex(NewsTableHelper.NEWS_SOURCE));
-		if (source.equals(NewsService.RSS_ETS)) {
-			holder.logo.setBackgroundDrawable(webLogo);
-		}
-		if (source.equals(NewsService.FACEBOOK)) {
-			holder.logo.setBackgroundDrawable(facebookLogo);
-		}
-		if (source.equals(NewsService.TWITTER)) {
-			holder.logo.setBackgroundDrawable(twitterLogo);
-		}
-
-		view.setTag(R.string.viewholderidtag,
-				cursor.getInt(cursor.getColumnIndex(NewsTableHelper.NEWS_ID)));
+	source = cursor.getString(cursor.getColumnIndex(NewsTableHelper.NEWS_SOURCE));
+	if (source.equals(NewsService.RSS_ETS)) {
+	    holder.logo.setBackgroundDrawable(webLogo);
+	}
+	if (source.equals(NewsService.FACEBOOK)) {
+	    holder.logo.setBackgroundDrawable(facebookLogo);
+	}
+	if (source.equals(NewsService.TWITTER)) {
+	    holder.logo.setBackgroundDrawable(twitterLogo);
+	}
+	if (source.equals(NewsService.INTERFACE)) {
+	    holder.logo.setBackgroundDrawable(interfaceLogo);
 	}
 
-	@Override
-	public View newView(final Context context, final Cursor cursor,
-			final ViewGroup parent) {
-		final LayoutInflater inflater = LayoutInflater.from(context);
-		final View v = inflater.inflate(R.layout.news_list_item, null);
-		bindView(v, context, cursor);
-		return v;
-	}
+	view.setTag(R.string.viewholderidtag,
+		cursor.getInt(cursor.getColumnIndex(NewsTableHelper.NEWS_ID)));
+    }
+
+    @Override
+    public View newView(final Context context, final Cursor cursor, final ViewGroup parent) {
+	final LayoutInflater inflater = LayoutInflater.from(context);
+	final View v = inflater.inflate(R.layout.news_list_item, null);
+	bindView(v, context, cursor);
+	return v;
+    }
 }
